24aa9683976e77e5fcdd1e2af348bdc66dd21ce3
edbarnes01 <eajbarnes1@gmail.com>
2020-12-29 15:25:28 +0000

    Initial Commit

---
+++ DawnsFlowerBox/DawnsFlowerBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawnsFlowerBoxApp.swift
+//  DawnsFlowerBox
+//
+//  Created by Ed Barnes on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct DawnsFlowerBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
